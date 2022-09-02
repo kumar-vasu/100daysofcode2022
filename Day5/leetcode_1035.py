4c4a01ddc68a89b5b751a820aa5a8738cc87ffbf
kumar-vasu <75114390+kumar-vasu@users.noreply.github.com>
2022-09-02 08:08:58 +0530

    day5 updates

---
+++ Day5/leetcode_1035.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxUncrossedLines(self, text1: List[int], text2: List[int]) -> int:
+        
+        dp = [[0 for i in range(len(text1) + 1)] for j in range(len(text2) + 1)]
+
+        for i in range(len(dp)):
+            for j in range(len(dp[0])):
+
+                if i == 0  or j == 0:
+                    continue
+
+                if text2[i-1] == text1[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
+        return dp[-1][-1]
