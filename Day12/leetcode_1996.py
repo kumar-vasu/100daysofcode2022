c0b9cd549128d84747aa74fdad8a563a14614239
kumar-vasu <75114390+kumar-vasu@users.noreply.github.com>
2022-09-09 12:06:00 +0530

    Day 12 updates

--- Day12/leetcode_1996.py
+++ Day12/leetcode_1996.py
@@ -1,17 +1,19 @@
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         
-        properties = list(reversed(sorted(properties, key = lambda x:x[0])))
+        properties = list(reversed(sorted(properties, key = lambda x:[x[0],-x[1]])))
         
         weak = 0
         
         at = properties[0][0]
         de = properties[0][1]
         
+        
         for i in range(1, len(properties)):
-            if properties[i][0] < at and properties[i][1] < de:
+            
+            if properties[i][1] < de and properties[i][0] < at:
                 weak += 1
-            else:
+            if properties[i][1] > de:
                 at = properties[i][0]
                 de = properties[i][1]
         
